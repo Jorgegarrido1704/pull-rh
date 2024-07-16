8f2f4c3a3a73536d60d9673d14ad74847ebad6a9
Jorge <garridofth@outlook.com>
2024-07-16 14:19:01 -0600

    update 1607242

--- herramental/urls.py
+++ herramental/urls.py
@@ -1,6 +1,6 @@
-from views import *
+from . import views
 from django.urls import path
-from django.contrib.auth import views as auth_views
+
 urlpatterns = [
-    path('diarios/', view.diarios, name='diarios'),
+    path('diarios/', views.diarios, name='diarios'),
 ]
