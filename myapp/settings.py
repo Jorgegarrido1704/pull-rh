021e678521770cab8266dcbcf97384c223f32be9
Jorge G <jgarrido@mx.bergstrominc.com>
2025-08-09 14:35:26 -0500

    ese

--- myapp/settings.py
+++ myapp/settings.py
@@ -16,7 +16,8 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = str(Path(__file__).resolve().parent.parent)
 
-
+# Fix for LiveSync
+BASE_DIR_STR = str(BASE_DIR)
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
